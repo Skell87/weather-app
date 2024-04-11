63d4b6f8cbd21398730bfbc2387717a72ad239f8
Brandon Heisler <brandon.heisler@gmail.com>
2024-04-11 15:19:04 -0400

    adding boxes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,66 @@
 import axios from "axios";
 console.log('main.js loaded');
+    // get weather button as a constant, creating a 'button' element in our document.
+const weatherButton = document.createElement('button');
+    // the text content of the button is get weather.
+weatherButton.textContent = 'Get Weather.';
+    // this appends the button to the child of body
+document.body.appendChild(weatherButton);
 
-    // have to set up an "if failed" for the weather api
+// this is the app body design.
+const mainBody = document.createElement('div');
+mainBody.classList.add('mainBodyStyle');
+document.body.append(mainBody);
+
+// first div for city box
+const cityBox = document.createElement('div');
+cityBox.classList.add('cityBoxStyle');
+mainBody.append(cityBox);
+
+// this is the tag that says "city" in orange
+const cityTag = document.createElement('div');
+cityTag.classList.add('cityTagStyle');
+cityBox.append(cityTag);
+
+
+// this is the input for the city 
+const cityInput = document.createElement('div');
+cityInput.classList.add('cityInputStyle');
+cityBox.append(cityInput);
+
+
+// this is the parent temp box
+const tempBox = document.createElement('div');
+tempBox.classList.add('tempBoxStyle');
+mainBody.append(tempBox);
+
+//this is the tag that says "temperature" in orange
+const tempTag = document.createElement('div');
+tempTag.classList.add('tempTagStyle');
+tempBox.append(tempTag);
+
+//this is a box justifier for the 3 temp boxes
+const tempJustifier = document.createElement('div');
+tempJustifier.classList.add('tempJustifierStyle');
+tempBox.append(tempJustifier);
+
+//this is the kelvin box.
+const kelvinBox = document.createElement('div');
+kelvinBox.classList.add('kelvinBoxStyle');
+tempJustifier.append(kelvinBox);
+
+const fahrenheitBox = document.createElement('div');
+fahrenheitBox.classList.add('fahrenheitBoxStyle');
+tempJustifier.append(fahrenheitBox);
+
+const celsiusBox = document.createElement('div');
+celsiusBox.classList.add('celsiusBoxStyle');
+tempJustifier.append(celsiusBox);
+
+
+
+
+// geolocator and weather api.
     
     navigator.geolocation.getCurrentPosition((position) => {
         let latLong = [position.coords.latitude, position.coords.longitude];
@@ -12,13 +71,20 @@ console.log('main.js loaded');
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latLong[0]}&lon=${latLong[1]}&appid=58870a89a0e26793783ede860847d0fe`)
         .then((response) => {
             console.log(response.data);
+            console.log('this is temp',response.data.main.temp);
             console.log(response.status);
             console.log(response.statusText);
             console.log(response.headers);
             console.log(response.config);
+            // below button gets temp data.
+            weatherButton.addEventListener('click', () => {
+                alert(`the temp in kelvin is ${response.data.main.temp}`);
+            })
              });
          });
      
       console.log('end')
 
       // markup and add event listeners prior to axios call.
+
+  
